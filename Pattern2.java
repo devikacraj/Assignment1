7c0f03b8c8362b2982bd7f81088ef7e389022028
DEVIKA C RAJ <76911590+devikacraj@users.noreply.github.com>
2021-05-17 16:47:13 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -4,19 +4,23 @@ class Pattern2
 public static void main (String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.print("Enter the rows: ");
-int rows=sc.nextInt(); //get input from user
-for(int i=1; i<=rows; i++){//Print each row 
-for(int j=i; j<rows; j++){//Print space for Pyramid shape 
+int rows=sc.nextInt();
+for(int i=1; i<=rows; i++)
+{
+for(int j=i; j<rows; j++)
+{
 System.out.print(" "); 
 } 
-for(int k=1; k<2*i; k++){//print *
+for(int k=1; k<2*i; k++)
+{
 if(i==rows || (k==1 || k==2*i-1))
  System.out.print("*"); 
- else {
+ else 
+ {
  System.out.print(" "); 
  }
 }
- System.out.println(""); //move to next line
+ System.out.println("");
 }
 }
 }
